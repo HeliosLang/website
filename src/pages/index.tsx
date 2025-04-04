2225aed630366ac99aeb48094b364e8141bcde67
christianschmitz <cschmitz398@gmail.com>
2025-04-04 10:57:40 -0600

    - only warn upon detecting broken links - index.tsx contains all components related not used on other pages

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,7 +6,6 @@ import {type ColorMode, useColorMode} from '@docusaurus/theme-common'
 import { ColorModeProvider } from '@docusaurus/theme-common/internal'
 import Layout from '@theme/Layout'
 import Heading from '@theme/Heading'
-import { FeatureItem, features } from './features'
 
 export default function Home(): ReactNode {
   const {siteConfig} = useDocusaurusContext()
@@ -118,6 +117,46 @@ const StyledMain = styled.main<StyledMainProps>`
   justify-content: center;
   background-color: ${({$colorMode}) => $colorMode == "dark" ? "#242526" : "#eee"};
 `
+
+type FeatureItem = {
+  title: string
+  description: ReactNode
+}
+
+const features: FeatureItem[] = [
+{
+  title: 'Designed for audits',
+  description: (
+    <>
+      The Helios Domain Specific Language (DSL) is a simple C-like language, designed for readability and auditable.
+    </>
+  ),
+},
+{
+  title: 'Optimal UPLC',
+  description: (
+    <>
+      Helios has one of the most advanced compilers in Cardano, producing compact and efficient on-chain bytecode.
+    </>
+  ),
+},
+{
+  title: 'No external dependencies',
+  description: (
+    <>
+      The Helios compiler and tx-building SDK don't have external dependencies, minimizing supply chain risks.
+    </>
+  ),
+},
+/*{
+  title: 'No WASM',
+  description: (
+    <>
+      Helios doesn't use WASM, ensuring DApps built with Helios run anywhere.
+    </>
+  ),
+},*/
+]
 
 function HomepageFeatures(): ReactNode {
   return (
