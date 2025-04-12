5e077161a1a38bb070ecfd72ac0db41367444c93
christianschmitz <cschmitz398@gmail.com>
2025-04-11 23:25:38 -0600

    all js/ts symbols have a badge

--- build.sdk.mjs
+++ build.sdk.mjs
@@ -21,7 +21,7 @@ const ALL_PACKAGES = MAIN_PACKAGES.concat(PLUMBING_PACKAGES)
 
 // TODO: turn this into a docusaurus plugin so it can operate during hot-reloads
 /**
- * @import { Comment, CommentDisplayPart, ProjectReflection, SourceReference } from "typedoc"
+ * @import { Comment, CommentDisplayPart, DeclarationReflection, ParameterReflection, ProjectReflection, SignatureReflection, SomeType, SourceReference } from "typedoc"
  */
 
 async function main() {
@@ -45,6 +45,10 @@ function getPackageDocPath(pkgName) {
  * @param {string} symbolName 
  */
 function getSymbolDocPath(pkgName, symbolName) {
+    if (pkgName.startsWith("@helios-lang/")) {
+        pkgName = pkgName.slice(("@helios-lang/").length)
+    }
+
     const isSame = symbolName.toLowerCase() == pkgName.toLowerCase()
 
     const name = isSame ? `${symbolName}_` : symbolName
@@ -108,30 +112,359 @@ function writePackageDocs(pkgName) {
     ].join("\n"))
 
     for (let child of pkgDoc.children) {
-        const name = child.name
-
-        if (!child.sources || child.sources.length == 0) {
-            throw new Error("sources not set for " + name)
-        }
-
-        const site = child.sources[0]
-
-        let content = ""
-        if (child.comment) {
-            content = stringifyComment(pkgName, child.comment)
-        }
-
-        const symbolPath = getSymbolDocPath(pkgName, name)
-
-        writeFileSync(`.${symbolPath}.md`, [
-            "---",
-            `sidebar_label: ${name}`,
-            `custom_edit_url: ${getSymbolEditLink(pkgName, site)}`,
-            "---",
-            `# ${name}`,
-            content
-        ].join("\n"))
+        writeSymbolDoc(pkgName, child)
     }
+}
+
+/**
+ * @param {string} pkgName
+ * @param {DeclarationReflection} child
+ */
+function writeSymbolDoc(pkgName, child) {
+    switch (child.variant) {
+        case "declaration":
+            switch(child.kind) {
+                case 4:
+                    writeNamespaceDoc(pkgName, child)
+                    return
+                case 32:
+                    writeConstantDoc(pkgName, child)
+                    return
+                case 64:
+                    writeFunctionDoc(pkgName, child)
+                    return
+                case 128:
+                    writeClassDoc(pkgName, child)
+                    return
+                case 256:
+                    writeInterfaceDoc(pkgName, child)
+                    return
+                case 2097152:
+                    writeTypeAliasDoc(pkgName, child)
+                    return
+            }
+    }
+
+    writeGenericSymbolDoc(pkgName, child)
+}
+
+/**
+ * @param {string} pkgName 
+ * @param {DeclarationReflection} child 
+ * @returns {{name: string, path: string, site: SourceReference}}
+ */
+function getCommonSymbolInfo(pkgName, child) {
+    const name = child.name
+
+    const symbolPath = getSymbolDocPath(pkgName, name)
+
+    if (!child.sources || child.sources.length == 0) {
+        throw new Error("sources not set for " + name)
+    }
+
+    const site = child.sources[0]
+
+    return {name, path: symbolPath, site}
+}
+
+/**
+ * @param {string} pkgName 
+ * @param {DeclarationReflection} child 
+ */
+function writeConstantDoc(pkgName, child) {
+    const {name, path, site} = getCommonSymbolInfo(pkgName, child)
+
+    const comment = child.comment ? stringifyComment(pkgName, child.comment) : ""
+    const typeSnippet = `<CodeBlock className="language-ts">export const ${name}${child.defaultValue == "..." ? ": " + stringifyType(pkgName, child.type) : " = " + child.defaultValue}</CodeBlock>` 
+
+    const content = [
+        `# <span className="constant_badge">${name}</span>`,
+        comment,
+        typeSnippet
+    ]
+
+    writeFileSync(`.${path}.md`, [
+        
+        "---",
+        `title: ${name}`,
+        `sidebar_label: ${name}`,
+        `sidebar_class_name: constant_badge`,
+        `custom_edit_url: ${getSymbolEditLink(pkgName, site)}`,
+        "---",
+        "",
+        "import CodeBlock from '@theme/CodeBlock'",
+        "",
+        content.join("\n")
+    ].join("\n"))
+}
+
+/**
+ * @param {string} pkgName 
+ * @param {DeclarationReflection} child 
+ */
+function writeFunctionDoc(pkgName, child) {
+    const {name, path, site} = getCommonSymbolInfo(pkgName, child)
+
+    const content = [
+        `# <span className="function_badge">${name}</span>`,
+    ]
+
+    const overloads = child.signatures ?? []
+
+    for (let i = 0; i < overloads.length; i++) {
+        const overload = overloads[i]
+
+        if (overloads.length > 1) {
+            content.push(`\n## Overload ${i+1}`)
+        }
+
+        const comment = overload.comment ? stringifyComment(pkgName, overload.comment) : ""
+        content.push(comment)
+
+        const returnType = stringifyType(pkgName, overload.type)
+        const typeSnippet = `<CodeBlock className="language-ts">export function ${name}(${stringifyFunctionParams(pkgName, overload.parameters ?? [])}): ${returnType}</CodeBlock>`
+        content.push(typeSnippet)
+    }
+
+    writeFileSync(`.${path}.md`, [
+        
+        "---",
+        `title: ${name}`,
+        `sidebar_label: ${name}`,
+        `sidebar_class_name: function_badge`,
+        `custom_edit_url: ${getSymbolEditLink(pkgName, site)}`,
+        "---",
+        "",
+        "import CodeBlock from '@theme/CodeBlock'",
+        "",
+        content.join("\n")
+    ].join("\n"))
+}
+
+/**
+ * @param {string} pkgName 
+ * @param {DeclarationReflection} child 
+ */
+function writeInterfaceDoc(pkgName, child) {
+    const {name, path, site} = getCommonSymbolInfo(pkgName, child)
+
+    const comment = child.comment ? stringifyComment(pkgName, child.comment) : ""
+    //const typeSnippet = `<CodeBlock className="language-ts">export const ${name}${child.defaultValue == "..." ? ": " + stringifyType(pkgName, child.type) : " = " + child.defaultValue}</CodeBlock>` 
+
+    const content = [
+        `# <span className="interface_badge">${name}</span>`,
+        comment,
+      //  typeSnippet
+    ]
+
+    writeFileSync(`.${path}.md`, [
+        
+        "---",
+        `title: ${name}`,
+        `sidebar_label: ${name}`,
+        `sidebar_class_name: interface_badge`,
+        `custom_edit_url: ${getSymbolEditLink(pkgName, site)}`,
+        "---",
+        "",
+        "import CodeBlock from '@theme/CodeBlock'",
+        "",
+        content.join("\n")
+    ].join("\n"))
+}
+
+/**
+ * @param {string} pkgName 
+ * @param {DeclarationReflection} child 
+ */
+function writeNamespaceDoc(pkgName, child) {
+    const {name, path, site} = getCommonSymbolInfo(pkgName, child)
+
+    const comment = child.comment ? stringifyComment(pkgName, child.comment) : ""
+    //const typeSnippet = `<CodeBlock className="language-ts">export const ${name}${child.defaultValue == "..." ? ": " + stringifyType(pkgName, child.type) : " = " + child.defaultValue}</CodeBlock>` 
+
+    const content = [
+        `# <span className="namespace_badge">${name}</span>`,
+        comment,
+      //  typeSnippet
+    ]
+
+    writeFileSync(`.${path}.md`, [
+        
+        "---",
+        `title: ${name}`,
+        `sidebar_label: ${name}`,
+        `sidebar_class_name: namespace_badge`,
+        `custom_edit_url: ${getSymbolEditLink(pkgName, site)}`,
+        "---",
+        "",
+        "import CodeBlock from '@theme/CodeBlock'",
+        "",
+        content.join("\n")
+    ].join("\n"))
+}
+
+/**
+ * @param {string} pkgName 
+ * @param {DeclarationReflection} child 
+ */
+function writeClassDoc(pkgName, child) {
+    const {name, path, site} = getCommonSymbolInfo(pkgName, child)
+
+    const comment = child.comment ? stringifyComment(pkgName, child.comment) : ""
+    //const typeSnippet = `<CodeBlock className="language-ts">export const ${name}${child.defaultValue == "..." ? ": " + stringifyType(pkgName, child.type) : " = " + child.defaultValue}</CodeBlock>` 
+
+    const content = [
+        `# <span className="class_badge">${name}</span>`,
+        comment,
+      //  typeSnippet
+    ]
+
+    writeFileSync(`.${path}.md`, [
+        
+        "---",
+        `title: ${name}`,
+        `sidebar_label: ${name}`,
+        `sidebar_class_name: class_badge`,
+        `custom_edit_url: ${getSymbolEditLink(pkgName, site)}`,
+        "---",
+        "",
+        "import CodeBlock from '@theme/CodeBlock'",
+        "",
+        content.join("\n")
+    ].join("\n"))
+}
+
+/**
+ * @param {string} pkgName 
+ * @param {DeclarationReflection} child 
+ */
+function writeTypeAliasDoc(pkgName, child) {
+    const {name, path, site} = getCommonSymbolInfo(pkgName, child)
+
+    const comment = child.comment ? stringifyComment(pkgName, child.comment) : ""
+    const typeSnippet = `<CodeBlock className="language-ts">export type ${name} = ${stringifyType(pkgName, child.type)}</CodeBlock>` 
+
+    const content = [
+        `# <span className="type_badge">${name}</span>`,
+        comment,
+        typeSnippet
+    ]
+
+    writeFileSync(`.${path}.md`, [
+        
+        "---",
+        `title: ${name}`,
+        `sidebar_label: ${name}`,
+        `sidebar_class_name: type_badge`,
+        `custom_edit_url: ${getSymbolEditLink(pkgName, site)}`,
+        "---",
+        "",
+        "import CodeBlock from '@theme/CodeBlock'",
+        "",
+        content.join("\n")
+    ].join("\n"))
+}
+
+/**
+ * @param {string} pkgName 
+ * @param {ParameterReflection[]} params 
+ * @returns {string}
+ */
+function stringifyFunctionParams(pkgName, params) {
+    if (params.length == 0) {
+        return ""
+    } else if (params.length == 1) {
+        return `${params[0].name}: ${stringifyType(pkgName, params[0].type)}`
+    } else {
+        return "\n&nbsp;&nbsp;" + params.map(p => `${p.name}: ${stringifyType(pkgName, p.type)}`).join(",\n&nbsp;&nbsp;") + "\n"
+    }
+}
+
+/**
+ * @param {string} pkgName
+ * @param {SomeType | undefined} t 
+ */
+function stringifyType(pkgName, t) {
+    if (!t) {
+        return "unknown"
+    } else {
+        switch(t.type) {
+            case "array":
+                return `${stringifyType(pkgName, t.elementType)}[]`
+            case "unknown":
+            case "intrinsic":
+                return t.name
+            case "reference":
+                const typeParams = t.typeArguments ? `&lt;${t.typeArguments.map(ta => stringifyType(pkgName, ta)).join(", ")}>` : ""
+                if (t.package == "typescript") {
+                    switch(t.name) {
+                        case "Uint8Array":
+                            return `[Uint8Array](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Uint8Array)`
+                        default:
+                            return t.name + typeParams
+                    }
+                } else {
+                    const path = getSymbolDocPath(t.package ?? pkgName, t.name)
+                    return `[${t.name}](${path})` + typeParams
+                }
+            case "reflection":
+                if (t.declaration.children) {
+                    return `\\{${t.declaration.children.map(c => {
+                        const key = `${c.name}${c.flags.isOptional ? "?" : ""}`
+                        const value = c.type ? stringifyType(pkgName, c.type) : c.signatures && c.signatures.length > 0 ? stringifyFunctionSignature(pkgName, c.signatures[0]): "unknown"
+                        return `${key}: ${value}`
+                    }).join(", ")}\\}`
+                } else if (t.declaration.signatures && t.declaration.signatures.length > 0) {
+                    const signature = t.declaration.signatures[0]
+
+                    return stringifyFunctionSignature(pkgName, signature)
+                } else {
+                    return "unknown"
+                }
+            case "union":
+                return t.types.map(ut => stringifyType(pkgName, ut)).join(" | ")
+            case "intersection":
+                return t.types.map(it => stringifyType(pkgName, it)).join(" & ")
+            case "literal":
+                if (typeof t.value == "string") {
+                    return `"${t.value.replaceAll("{", "\\{")}"`
+                } else {
+                    return t.value
+                }
+            default: 
+                return "unknown"
+        }
+    }
+}
+
+/**
+ * @param {string} pkgName 
+ * @param {SignatureReflection} signature 
+ * @returns {string}
+ */
+function stringifyFunctionSignature(pkgName, signature) {
+    return `(${stringifyFunctionParams(pkgName, signature.parameters ?? [])}) => ${stringifyType(pkgName, signature.type)}`
+}
+/**
+ * @param {string} pkgName
+ * @param {DeclarationReflection} child 
+ */
+function writeGenericSymbolDoc(pkgName, child) {
+    const {name, path, site} = getCommonSymbolInfo(pkgName, child)
+
+    let content = ""
+    if (child.comment) {
+        content = stringifyComment(pkgName, child.comment)
+    }
+
+    writeFileSync(`.${path}.md`, [
+        "---",
+        `sidebar_label: ${name}`,
+        `custom_edit_url: ${getSymbolEditLink(pkgName, site)}`,
+        "---",
+        `# ${name}`,
+        content
+    ].join("\n"))
 }
 
 /**
